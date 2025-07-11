60b9f2c024630b4921fc09bd7017b548dd24e34b
Sumit Ghewade <ghewadesumit@gmail.com>
2025-07-11 19:00:54 -0400

    feat: added black background to the dropdown

--- src/components/AddQuestion/AddQuestion.jsx
+++ src/components/AddQuestion/AddQuestion.jsx
@@ -242,9 +242,7 @@ const AddQuestion = ({
                 value={formData.questionSection}
                 onChange={handleInputChange}
                 required
-                className={`w-full px-4 py-3 border border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 ${getDifficultyColor(
-                  formData.questionDifficulty
-                )} bg-gray-800`}
+                className={`w-full px-4 py-3 border text-white border-gray-700 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200  bg-gray-800`}
               >
                 {questionSectionsData?.map((item) => (
                   <option key={item.id} value={item.id}>
